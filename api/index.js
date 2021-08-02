47e510c7deb7026d469626773b9c3625bdb4c836
Elijah Micho <olum.micho@gmail.com>
2021-08-02 11:06:28 -0600

    fixed missing middleware

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const axios = require('axios');
+const cors = require('cors');
 const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
