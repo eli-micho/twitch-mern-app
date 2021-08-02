9932411c871047b7cc83403d6e718eaf48074184
Elijah Micho <olum.micho@gmail.com>
2021-08-02 11:04:20 -0600

    updated build scripts

--- api/index.js
+++ api/index.js
@@ -7,12 +7,29 @@ const morgan = require("morgan");
 const channelRoute = require("./routes/channels")
 
 //middlewares
+app.use(cors());
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
 
+//Route
 app.use("/channels", channelRoute);
 
-app.listen(4400, () => {
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    if ('OPTIONS' == req.method) {
+        res.send(200);
+      }
+      else {
+        next();
+      }
+});
+
+//Test for heroku
+/* app.get('/', (req, res) => { res.send('Hello from Express!')})
+ */
+
+app.listen(process.env.PORT || 4400, () => {
     console.log("Backend server is running!")
 });
