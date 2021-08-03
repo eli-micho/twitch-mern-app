6fd94a7c7ce1ff843579f5d25610945a8ba74c79
Elijah Micho <olum.micho@gmail.com>
2021-08-03 12:32:34 -0600

    added procfile

--- api/index.js
+++ api/index.js
@@ -26,12 +26,6 @@ app.use(function(req, res, next) {
       }
 });
 
-if (process.env.NODE_ENV === "production"{
-  app.use(express.static("build"));
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-  });
-}
 
 //Test for heroku
 app.get('/', (req, res) => { res.send('Hello from Express!')})
